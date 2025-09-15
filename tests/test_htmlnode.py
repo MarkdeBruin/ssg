14cfde45aeab8f9d144e22a7102b1a2ca0b45da9
MarkdeBruin <contact@mrkdbrn.com>
2025-09-15 11:14:07 +0200

    Leafnode with tests

--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -36,6 +36,25 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(parent.children[0], child)
         self.assertEqual(parent.children[0].tag, "span")
         self.assertEqual(parent.children[0].value, "child")
+    
+    #Leafnode tests
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+    
+    def test_leaf_no_value_raises(self):
+        with self.assertRaises(ValueError):
+            LeafNode("p", None)
+
 
 
 if __name__ == "__main__":
